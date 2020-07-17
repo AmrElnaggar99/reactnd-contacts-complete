4e48de7d204a4fda9ca3b2675592736b50d65175
Amr Elnaggar <amr.eln2gar@gmail.com>
2020-07-17 04:12:00 +0200

    Added styles and functionality to the delete button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  removeContact = (contactToDelete) => {
+    this.setState((state) => {
+      contacts: state.contacts.filter((contact) => contact.id !== contactToDelete.id)
+    })
+  }
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
       </div>
     )
   }
